610a49d13546cf9d03bf47889e80575152c6a039
Nathan Quadros da Silva <contato@nathanqsilva.com.br>
2022-05-16 14:39:27 -0300

    criação do footer

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,3 +1,4 @@
+import { FacebookLogo, InstagramLogo, YoutubeLogo } from 'phosphor-react'
 import { Link, Outlet } from 'react-router-dom'
 
 export function Header() {
@@ -38,11 +39,63 @@ export function Header() {
                     </ul>
                 </div>
             </header>
+
             <main>
                 <Outlet />
             </main>
+            
             <footer>
-                
+                <div className=''>
+                    <section className='bg-vavaCinza-100 flex flex-row justify-around text-slate-100 py-6 font-light'>
+                        <div className='w-3/12 flex items-center flex-col'>
+                            <p className='pb-2'>DEPARTAMENTOS</p>
+                            <a href="" className='text-sm font-light'>SKINS</a>
+                            <a href="" className='text-sm font-light'>PROMOÇÕES</a>
+                            <a href="" className='text-sm font-light'>NOVIDADES</a>
+                            <a href="" className='text-sm font-light'>MAIS VENDIDAS</a>
+                            <a href="" className='text-sm font-light'>VEM POR AI</a>
+                        </div>
+                        <div className='w-3/12 flex items-center flex-col'>
+                            <p className='pb-2'>INSTITUCIONAIS</p>
+                            <a href="" className='text-sm font-light'>SOBRE NÓS</a>
+                            <a href="" className='text-sm font-light'>POLITICAS</a>
+                            <a href="" className='text-sm font-light'>TRABALHE CONOSCO</a>
+                            <a href="" className='text-sm font-light'>MAPA DO SITE</a>
+                        </div>
+                        <div className='w-3/12 flex items-center flex-col'>
+                            <p className='pb-2'>CONTATO</p>
+                            <a href="" className='text-sm font-light'>CENTRAL/SAC - (XX) XXXX-XXXX</a>
+                            <a href="" className='text-sm font-light'>EMAIL - CONTATO@SKINSVA.COM.BR</a>
+                            <a href="" className='text-sm font-light'>ATENDIMENTO DAS 08:00 ÀS 20:00</a>
+                            <a href="" className='text-sm font-light'>SEGUNDA A SÁBADO</a>
+                            <a href="" className='text-sm font-light'>(EXCETO DOMINGO E FERIADOS)</a>
+                        </div>
+                        <div className='w-3/12 flex items-center flex-col'>
+                            <p className='pb-2'>MINHA CONTA</p>
+                            <a href="" className='text-sm font-light'>MEUS PEDIDOS</a>
+                            <a href="" className='text-sm font-light'>FAVORITOS</a>
+                        </div>
+                    </section>
+                </div>
+                <div className='flex items-center justify-center h-14 gap-6'>
+                    <button>
+                        <FacebookLogo size={48} color="#F2F2F2" />
+                    </button>
+                    <button>
+                        <InstagramLogo size={48} color="#F2F2F2" />
+                    </button>
+                    <button>
+                        <YoutubeLogo size={48} color="#F2F2F2" />
+                    </button>
+                </div>
+                <div className="bg-vavaCinza-100 font-normal flex items-center justify-center flex-col p-2">
+                    <p className="text-slate-100">
+                        SKINSVA é uma marca fictícia de ALUNOS UTFPR | RAs: a2164485 e axxxxxxx | Todos os direitos reservados. Os preços anunciados neste site são meramente imaginários.
+                    </p>
+                    <p className="text-slate-100">
+                        O skinsva! não é responsável por mal uso deste projeto. As fotos contidas nesta página são meramente ilustrativas do produto retiradas de valorant.fandom.com Este site não trabalha com criptografia SSL.
+                    </p>
+                </div>
             </footer>
         </>
     )
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,14 +4,14 @@ export default function HomePage() {
   return (
     <>
       <section className="p-12">
-        <div className='flex flex-col items-center bg-image-vava'>
+        <div className='flex flex-col items-center'>
           <h2 className="text-slate-100 font-valorant text-3xl border-b-2 border-vavaRed-100 mb-5">
             Se liga nas novidades
           </h2>
           <CarouselComponent />
         </div>
       </section>
-      <section className="flex flex-col items-center content-center bg-slate-700 ">
+      <section className="flex flex-col items-center content-center ">
         <div className=" flex flex-row items-center content-center my-8">
           <h1 className=" text-slate-300 text-2xl pr-10 border-r-4 border-red-500">Mais Vendidos</h1>
           <h2 className="text-slate-300 text-xl ml-10 "> Ver mais...</h2>
