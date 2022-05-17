5731c4a0ee0eabba0f2b022a5fcfefea8cda1877
Nathan Quadros da Silva <contato@nathanqsilva.com.br>
2022-05-17 11:38:30 -0300

    imagem background

--- src/Components/CardProduto.tsx
+++ src/Components/CardProduto.tsx
@@ -10,10 +10,10 @@ type PropsType = {
 
 export default function CardProduto(Props: PropsType) {
     return (
-        <div className="h-60 w-96 bg-vavaAzul-100 rounded-xl flex flex-col items-center justify-between">
+        <div className="h-60 w-96 bg-vavaAzul-100 rounded-xl flex flex-col items-center justify-between relative">
             <div className="flex justify-center items-center h-5/6">
                 <img className="w-3/4" src={Props.imagem} alt="" />
-                <HeartStraight className=""/>
+                <HeartStraight size={25} weight="bold" color="white" className="absolute right-3 top-2"/>
             </div>
             
             <div className="flex justify-around bg-vavaCinza-100 w-full h-1/6 rounded-b-2xl items-center text-slate-100  ">
