069e0f8a6f9fbcc44c61799b86a0c57e5f982195
Nathan Quadros da Silva <contato@nathanqsilva.com.br>
2022-05-30 16:06:37 -0300

    att

--- src/Components/Formulario.tsx
+++ src/Components/Formulario.tsx
@@ -12,39 +12,60 @@ export default function Formulario() {
     const onSubmit = (data: any) => console.log(data)
 
     return (
-        <div className="border w-9/12">
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <h3>Contact Form</h3>
+        <div className="w-9/12 flex justify-center">
+            <form onSubmit={handleSubmit(onSubmit)} className="flex justify-center flex-col text-slate-200">
+                <div className="flex flex-row justify-center gap-10">
+                    <div>
+                        <div className="flex flex-col mb-4">
+                            <label className="text-lg">Nome:</label>
+                            <input
+                                className="text-slate-900 "
+                                placeholder="Nome"
+                                type="text"
+                                {...register('nome', {
+                                    required : true
+                                })}
+                            />
+                            {errors.nome && <p>Seu nome é importante para continuar</p>}
+                        </div>
+
+                        <div className="flex flex-col mb-4">
+                            <label className="text-lg">E-mail:</label>
+                            <input
+                                type="email"
+                                placeholder="E-mail"
+                                {...register('email', {
+                                    required: true,
+                                    pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+                                })}
+                            />
+                            {errors.email && <p>Informe um email válido para continuar</p>}
+                        </div>
+                        <div className="flex flex-col mb-4">
+                            <label className="text-lg">Assunto:</label>
+                            <input
+                                type="text"
+                                placeholder="Assunto"
+                                {...register('assunto', {
+                                    required: true
+                                })}
+                            />
+                            {errors.assunto && <p>Assunto não pode ser vazio</p>}
+                        </div>
+                    </div>
+                    <div className="flex flex-col mb-4">
+                        <label className="text-lg">Mensagem:</label>
+                        <input
+                            type="text"
+                            placeholder="Mensagem"
+                            {...register('mensagem', {
+                                required: true
+                            })}
+                        />
+                        {errors.mensagem && <p>Mensagem não pode ser vazio</p>}
+                    </div>
                 </div>
-                <div>
-                    <input
-                        type="text"
-                        {...register('name')}
-                    />
-                </div>
-                <div>
-                    <input
-                        type="text"
-                        {...register('sobrenome', {
-                            required: true
-                        })}
-                    />
-                    {errors.sobrenome && <p>Last name is required.</p>}
-                </div>
-                <div>
-                    <input
-                        type="text"
-                        {...register('idade', {
-                            required: true,
-                            pattern: /\d+/
-                        })}
-                    />
-                    {errors.idade && <p>Age is required.</p>}
-                </div>
-                <div>
-                    <button>Submit Contact</button>
-                </div>
+                <button>Submit Contact</button>
             </form>
         </div>
     )
